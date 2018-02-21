a946736f2e89c37112b31e36881aff38da5edfd3
JasonSkillman <35405997+JasonSkillman@users.noreply.github.com>
2018-02-20 22:44:16 -0500

    Add files via upload

---
+++ Secure Folder/SecureFolderForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Secure_Folder
+{
+    partial class SecureFolderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SecureFolderForm));
+			this.HideBtn = new System.Windows.Forms.Button();
+			this.ShowBtn = new System.Windows.Forms.Button();
+			this.textBoxPassword = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// HideBtn
+			// 
+			this.HideBtn.Enabled = false;
+			this.HideBtn.Location = new System.Drawing.Point(12, 111);
+			this.HideBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.HideBtn.Name = "HideBtn";
+			this.HideBtn.Size = new System.Drawing.Size(88, 28);
+			this.HideBtn.TabIndex = 1;
+			this.HideBtn.Text = "Hide";
+			this.HideBtn.UseVisualStyleBackColor = true;
+			this.HideBtn.Click += new System.EventHandler(this.ClickFolderHide);
+			// 
+			// ShowBtn
+			// 
+			this.ShowBtn.Enabled = false;
+			this.ShowBtn.Location = new System.Drawing.Point(106, 111);
+			this.ShowBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.ShowBtn.Name = "ShowBtn";
+			this.ShowBtn.Size = new System.Drawing.Size(88, 28);
+			this.ShowBtn.TabIndex = 2;
+			this.ShowBtn.Text = "Show";
+			this.ShowBtn.UseVisualStyleBackColor = true;
+			this.ShowBtn.Click += new System.EventHandler(this.ClickFolderShow);
+			// 
+			// textBoxPassword
+			// 
+			this.textBoxPassword.Location = new System.Drawing.Point(12, 40);
+			this.textBoxPassword.Name = "textBoxPassword";
+			this.textBoxPassword.PasswordChar = '*';
+			this.textBoxPassword.Size = new System.Drawing.Size(182, 22);
+			this.textBoxPassword.TabIndex = 3;
+			this.textBoxPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ButtonKeyUp);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(266, 17);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Please enter password for Secure Folder";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(171, 17);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Secure Folder Commands";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(200, 37);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(88, 28);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Enter";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.ClickPasswordCheck);
+			// 
+			// SecureFolderForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(335, 164);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxPassword);
+			this.Controls.Add(this.ShowBtn);
+			this.Controls.Add(this.HideBtn);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "SecureFolderForm";
+			this.Text = "Secure Folder";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+		private System.Windows.Forms.Button HideBtn;
+		private System.Windows.Forms.Button ShowBtn;
+		private System.Windows.Forms.TextBox textBoxPassword;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Secure Folder/SecureFolderForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Secure_Folder {
+	public partial class SecureFolderForm : Form {
+
+		string pathCurrentProgram;
+		string pathTopLevel;
+		string pathFolder;
+
+		string password = "LOK";
+
+		DirectoryInfo dirSecureFolder;
+
+
+		public SecureFolderForm() {
+			InitializeComponent();
+
+			//Set the variables
+			pathCurrentProgram = Directory.GetCurrentDirectory();
+			//pathTopLevel = @"d:\Test";
+			pathFolder = Path.Combine(pathCurrentProgram, "Secure Folder");
+			
+			//Create the subfolders
+			Directory.CreateDirectory(pathFolder);
+
+			//Create a directory object
+			dirSecureFolder = new DirectoryInfo(pathFolder);
+		}
+
+
+
+		private void ButtonKeyUp(object sender, KeyEventArgs e) {
+			if(e.KeyCode == Keys.Enter) {
+				CheckPassword();
+			}
+		}
+		private void ClickPasswordCheck(object sender, EventArgs e) {
+			CheckPassword();
+		}
+
+		private void CheckPassword() {
+			if(textBoxPassword.Text.Equals(password)) {
+				HideBtn.Enabled = true;
+				ShowBtn.Enabled = true;
+			}
+			else {
+				HideBtn.Enabled = false;
+				ShowBtn.Enabled = false;
+				CreatePopUpMessage("You have entered the wrong password!", "Wrong Password");
+			}
+		}
+
+
+
+		private void ClickFolderHide(object sender, EventArgs e) {
+			FolderHide(dirSecureFolder);
+		}
+		private void ClickFolderShow(object sender, EventArgs e) {
+			FolderShow(dirSecureFolder);
+		}
+
+		private void FolderHide(DirectoryInfo dirFolder) {
+			//dirFolder.Attributes |= FileAttributes.Hidden;
+			dirFolder.Attributes = FileAttributes.Hidden;
+		}
+		private void FolderShow(DirectoryInfo dirFolder) {
+			//dirFolder.Attributes &= ~FileAttributes.Hidden;
+			dirFolder.Attributes = FileAttributes.Normal;
+		}
+
+		private void CreatePopUpMessage(string message, string title) {
+			string text = message;
+			string caption = title;
+			MessageBoxButtons buttons;
+			DialogResult result;
+
+			buttons = MessageBoxButtons.OK;
+			result = MessageBox.Show(text, caption, buttons);
+			/*
+			if(result == System.Windows.Forms.DialogResult.Yes) {
+				// Closes the parent form.
+				this.Close();
+			}
+			*/
+		}
+	}
+}
+
+/*
+//Checks to see if the folder exists
+if(!Directory.Exists(dirFolder.FullName)) {
+	//Doesn't create any parent directories that might be missing
+	Directory.CreateDirectory(dirFolder.Extension);
+	CreatePopUpMessage("Folder does not exist.", "No Folder");
+}
+else {
+	CreatePopUpMessage("Folder already exists.", "Folder");
+}
+*/
+
+/*
+private void ClickButton(object sender, EventArgs e) {
+			
+	// Create a file name for the file you want to create. 
+	string fileName = Path.GetRandomFileName();
+
+	// This example uses a random string for the name, but you also can specify
+	// a particular name.
+	//string fileName = "MyNewFile.txt";
+
+	// Use Combine again to add the file name to the path.
+	pathFolder = Path.Combine(pathFolder, fileName);
+
+	// Verify the path that you have constructed.
+	Console.WriteLine("Path to my file: {0}\n", pathFolder);
+
+	// Check that the file doesn't already exist. If it doesn't exist, create
+	// the file and write integers 0 - 99 to it.
+	// DANGER: System.IO.File.Create will overwrite the file if it already exists.
+	// This could happen even with random file names, although it is unlikely.
+	if(!File.Exists(pathFolder)) {
+		using(FileStream fs = File.Create(pathFolder)) {
+			for(byte i = 0; i < 100; i++) {
+				fs.WriteByte(i);
+			}
+		}
+	}
+	else {
+		Console.WriteLine("File \"{0}\" already exists.", fileName);
+		return;
+	}
+
+	// Read and display the data from your file.
+	try {
+		byte[] readBuffer = File.ReadAllBytes(pathFolder);
+		foreach(byte b in readBuffer) {
+			Console.Write(b + " ");
+		}
+		Console.WriteLine();
+	}
+	catch(IOException ee) {
+		Console.WriteLine(ee.Message);
+	}
+
+
+	CreatePopUpMessage("Test", "Title");
+
+	// Keep the console window open in debug mode.
+	//System.Console.WriteLine("Press any key to exit.");
+	//System.Console.ReadKey();
+
+}
+*/
